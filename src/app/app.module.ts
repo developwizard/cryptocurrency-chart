d076f0b2c6df16bd3e239db6a5fc574790ed138e
dewiz <churkin.it@gmail.com>
2023-05-30 22:46:07 +0300

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,18 @@ import {MatSelectModule} from "@angular/material/select";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {FormsModule} from "@angular/forms";
+import {CoinListComponent} from './coin-list/coin-list.component';
+import {CoinDetailComponent} from './coin-detail/coin-detail.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoinListComponent,
+    CoinDetailComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatInputModule,
