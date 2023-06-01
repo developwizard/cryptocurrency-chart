bb5c5caa249ac67fe674391e6bbcbcba3703d361
dewiz <churkin.it@gmail.com>
2023-06-01 23:03:55 +0300

    Implemented coin-list table info feature

--- src/app/coin-list/coin-list.component.ts
+++ src/app/coin-list/coin-list.component.ts
@@ -1,6 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ApiService} from "../service/api.service";
 import {CommonModule} from "@angular/common";
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
+import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
+import {MatSort, MatSortModule} from "@angular/material/sort";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 @Component({
   selector: 'app-coin-list',
@@ -8,11 +13,22 @@ import {CommonModule} from "@angular/common";
   styleUrls: ['./coin-list.component.css'],
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ]
 })
 export class CoinListComponent implements OnInit {
   bannerData: any;
+  dataSource!: MatTableDataSource<any>;
+  //api columns
+  displayedColumns: string[] = ['symbol', 'current_price', 'price_change_percentage_24h', 'market_cap'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   constructor(private apiService: ApiService) {
   }
 
@@ -31,7 +47,18 @@ export class CoinListComponent implements OnInit {
   getAllData() {
     this.apiService.getCurrencyData("EUR")
       .subscribe(res => {
-        console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
