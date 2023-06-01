da3a4f465b7f6eb974cd357f165b5c13aeb8a5f5
dewiz <churkin.it@gmail.com>
2023-06-02 00:26:16 +0300

    Implemented coin dynamic chart

--- src/app/coin-detail/coin-detail.component.ts
+++ src/app/coin-detail/coin-detail.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ApiService} from "../service/api.service";
 import {ActivatedRoute} from "@angular/router";
 import {CommonModule} from "@angular/common";
+import {ChartConfiguration, ChartType} from "chart.js";
+import {BaseChartDirective, NgChartsModule} from "ng2-charts";
 
 @Component({
   selector: 'app-coin-detail',
@@ -9,14 +11,43 @@ import {CommonModule} from "@angular/common";
   styleUrls: ['./coin-detail.component.css'],
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    NgChartsModule
   ]
 })
 export class CoinDetailComponent implements OnInit {
   coinData: any;
   coinId!: string;
-  days: number = 1;
+  days: number = 30;
   currency: string = "EUR";
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets: [{
+      data: [],
+      label: 'Price Trends',
+      backgroundColor: 'rgba(148, 159, 177, 0.2)',
+      borderColor: '#009688',
+      pointBackgroundColor: '#009688',
+      pointBorderColor: '#0099688',
+      pointHoverBackgroundColor: '#009688',
+      pointHoverBorderColor: '#009688'
+    }],
+    labels: []
+  };
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      point: {
+        radius: 1
+      }
+    },
+    plugins: {
+      legend: {
+        display: true
+      }
+    }
+  }
+  public lineChartType: ChartType = 'line';
+  @ViewChild(BaseChartDirective) lineChart!: BaseChartDirective;
+
   constructor(private apiService: ApiService, private route: ActivatedRoute) {
   }
 
@@ -24,6 +55,7 @@ export class CoinDetailComponent implements OnInit {
     this.route.params.subscribe(val => {
       this.coinId = val['id'];
       this.getCoinData();
+      this.getGraphData();
     });
   }
 
@@ -31,7 +63,25 @@ export class CoinDetailComponent implements OnInit {
     this.apiService.getCurrencyById(this.coinId)
       .subscribe(res => {
         this.coinData = res;
-        console.log(this.coinData);
+      });
+  }
+
+  getGraphData() {
+    this.apiService.getCurrencyChartData(this.coinId, "EUR", this.days)
+      .subscribe(res => {
+        setTimeout(() => {
+          this.lineChart.chart?.update()
+        });
+        this.lineChartData.datasets[0].data = res.prices.map((p: any) => {
+          return p[1];
+        });
+        this.lineChartData.labels = res.prices.map((p: any) => {
+          let date = new Date(p[0]);
+          let time = date.getHours() > 12 ?
+            `${date.getHours() - 12}: ${date.getMinutes()} PM` :
+            `${date.getHours()}: ${date.getMinutes()} AM`;
+          return this.days === 1 ? time : date.toDateString();
+        });
       });
   }
 }
